6b8514726a0a1d653b75a2f9a5b6684bdb4c5126
Kazunari93 <aooni0930@gmail.com>
2020-03-18 01:29:22 +0900

    Updates

---
+++ precache-manifest.edf1e1ec4ed07d73f688bff45e004644.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1efde4af68362b670c775fd05ae9ba",
+    "url": "/react_redux_thunk/index.html"
+  },
+  {
+    "revision": "8165fb8afcdade0eb6cd",
+    "url": "/react_redux_thunk/static/js/2.ea1e3b70.chunk.js"
+  },
+  {
+    "revision": "9f66c648088f6560f0b7aed5c691a722",
+    "url": "/react_redux_thunk/static/js/2.ea1e3b70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3ba30613f374cbd828",
+    "url": "/react_redux_thunk/static/js/main.e45a510f.chunk.js"
+  },
+  {
+    "revision": "b8e386c11bca5e8d5e01",
+    "url": "/react_redux_thunk/static/js/runtime-main.e27c98c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-redux-thunk/precache-manifest.97e5062665ee816c1cfc96612e622b00.js"
+  "/react_redux_thunk/precache-manifest.edf1e1ec4ed07d73f688bff45e004644.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-thunk/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_redux_thunk/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
